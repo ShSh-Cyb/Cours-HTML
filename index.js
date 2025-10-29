c2f1e904b40a96199843775916cb34d3bdfb283c
ShSh-Cyb <shoghijean@gmail.com>
2025-10-29 12:37:16 +0100

    cours js

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ alert("hello world");
 console.log("toto");
 
 // 1 - exemple de selecteur avec modification du style
-document
+document.querySelector("h1").style.backgroundColor = "yellow";
+
+function addition () {
+    let a = 10;
+    let b = 5;
+    return a + b;
+}
+console.log(addition());
+
+function multiplication () {
+    let a = 10;
+    let b = 5;
+    return a * b;
+}
+console.log(multiplication());
+
+let a = 10;
+let b = 5;
+console.log(a, b, "test");
+
+
